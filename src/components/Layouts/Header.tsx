24f0029259eb4c0019120259dfb94f9a830d4fc2
naufalrays <rayhannaufalherlano@gmail.com>
2025-07-08 08:12:55 +0700

    fix declared but its value never read v2

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import { toggleTheme, toggleSidebar } from "../../store/themeConfigSlice";
